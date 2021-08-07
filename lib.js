7d1d94e5fcb297636cb74f05d4d146edd82178d7
Mehmood Ul Hassan <35806708+mhassanrahi@users.noreply.github.com>
2021-08-07 15:00:48 +0500

    * Added sum function

--- lib.js
+++ lib.js
@@ -1,8 +1,11 @@
 module.exports = {
     absolute: (number) => {
-
         if (number > 0) return number;
         if (number < 0) return -number;
         return 0;
-    }
+    },
+
+    sum: (a, b) => {
+        return a + b;
+      }
 }
