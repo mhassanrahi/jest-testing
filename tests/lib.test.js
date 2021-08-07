b21a8e65e8256aff4cb39e03169d9e42ef7ad43c
Mehmood Ul Hassan <35806708+mhassanrahi@users.noreply.github.com>
2021-08-07 15:28:21 +0500

    * Grouped related tests together inside describe function

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,19 +1,23 @@
 const lib = require('../lib')
 
-test("absolute - should return a positive number if the input is positive", () => {
-    const result = lib.absolute(1)
-    expect(result).toBe(1)
+describe('absolute', () => {
+    it("should return a positive number if the input is positive", () => {
+        const result = lib.absolute(1)
+        expect(result).toBe(1)
+    })
+    
+    it("should return a positive number if the input is negative", () => {
+        const result = lib.absolute(-1)
+        expect(result).toBe(1)
+    })
+    
+    it("should return zero if the input is zero", () => {
+        const result = lib.absolute(0)
+        expect(result).toBe(0)
+    })
 })
 
-test("absolute - should return a positive number if the input is negative", () => {
-    const result = lib.absolute(-1)
-    expect(result).toBe(1)
-})
 
-test("absolute - should return zero if the input is zero", () => {
-    const result = lib.absolute(0)
-    expect(result).toBe(0)
-})
 
 test("sum - adds 1 + 2 to equal 3", () => {
     const result = lib.sum(1, 2)
