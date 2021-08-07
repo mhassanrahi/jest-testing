4c9e16bdba200b8cd48e74e964756034b33aae3b
Mehmood Ul Hassan <35806708+mhassanrahi@users.noreply.github.com>
2021-08-07 14:58:28 +0500

    * Wrote second and third test cases

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,6 +1,19 @@
 const lib = require('../lib')
+
 test("absolute - should return a positive number if the input is positive", () => {
 
     const result = lib.absolute(1)
     expect(result).toBe(1)
+})
+
+test("absolute - should return a positive number if the input is negative", () => {
+
+    const result = lib.absolute(-1)
+    expect(result).toBe(1)
+})
+
+test("absolute - should return zero if the input is zero", () => {
+
+    const result = lib.absolute(0)
+    expect(result).toBe(0)
 })
